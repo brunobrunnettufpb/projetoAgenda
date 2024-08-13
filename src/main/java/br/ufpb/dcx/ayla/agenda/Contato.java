ef27891db905af89b2bce51a9f979549cdfb5065
brunobrunnettufpb <bruno.mangueira@dcx.ufpb.br>
2024-08-13 09:41:43 -0300

    240813

--- src/main/java/br/ufpb/dcx/ayla/agenda/Contato.java
+++ src/main/java/br/ufpb/dcx/ayla/agenda/Contato.java
@@ -1,8 +1,67 @@
 package br.ufpb.dcx.ayla.agenda;
 
-public class Contato {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Contato implements Serializable {
     private String nome;
     private int diaAniversario;
     private int mesAniversario;
 
+    public Contato(String nome, int diaAniversario, int mesAniversario) {
+        this.nome = nome;
+        this.diaAniversario = diaAniversario;
+        this.mesAniversario = mesAniversario;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getDiaAniversario() {
+        return diaAniversario;
+    }
+
+    public void setDiaAniversario(int diaAniversario) {
+        this.diaAniversario = diaAniversario;
+    }
+
+    public int getMesAniversario() {
+        return mesAniversario;
+    }
+
+    public void setMesAniversario(int mesAniversario) {
+        this.mesAniversario = mesAniversario;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato de nome " + nome +
+                ", que aniversaria em " + diaAniversario + "/" + mesAniversario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contato contato = (Contato) o;
+
+        if (diaAniversario != contato.diaAniversario) return false;
+        if (mesAniversario != contato.mesAniversario) return false;
+        return Objects.equals(nome, contato.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = nome != null ? nome.hashCode() : 0;
+        result = 31 * result + diaAniversario;
+        result = 31 * result + mesAniversario;
+        return result;
+    }
 }
